581e9a10c5056821254ea2addd5d4d98cc93a7f6
Elina <esardaryan@touchtunes.com>
2024-04-30 14:12:31 -0400

    How to Observe Internet Connectivity in Android - Android Studio Tutorial
    Philipp Lackner
    https://www.youtube.com/watch?v=TzV0oCRDNfM&t=432s

---
+++ ObserveConnectivity/app/src/main/java/ca/elina/observeconnectivity/MainActivity.kt
@@ -0,0 +1,34 @@
+package ca.elina.observeconnectivity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import ca.elina.observeconnectivity.ui.theme.ObserveConnectivityTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var connectivityObserver: ConnectivityObserver
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        connectivityObserver = NetworkConnectivityObserver(applicationContext)
+        setContent {
+            ObserveConnectivityTheme {
+                val status by connectivityObserver.observe().collectAsState(
+                    initial = ConnectivityObserver.Status.Unavailable
+                )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Network status: $status")
+                }
+            }
+        }
+    }
+}
