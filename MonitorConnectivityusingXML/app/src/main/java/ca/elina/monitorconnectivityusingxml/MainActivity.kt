204ea15399e93e45f54daeba8bbaf9393f2d0ff2
Elina <esardaryan@touchtunes.com>
2024-05-07 01:01:49 -0400

    The app displays "No internet connection" when there is no connection and "Internet is on" when it is connected. Also, when pressed on the button the toast message will be displayed about internet connectivity.
    Sources used to create the app were:
    1. https://www.geeksforgeeks.org/how-to-check-internet-connection-in-kotlin/
    2. Handle Internet Connection, Section #14 from Udemy course
    https://www.udemy.com/course/modern-food-recipes-app-android-development-with-kotlin/learn/lecture/23043122#overview

--- MonitorConnectivityusingXML/app/src/main/java/ca/elina/monitorconnectivityusingxml/MainActivity.kt
+++ MonitorConnectivityusingXML/app/src/main/java/ca/elina/monitorconnectivityusingxml/MainActivity.kt
@@ -1,20 +1,54 @@
 package ca.elina.monitorconnectivityusingxml
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var networkListener: NetworkListener
+    private var networkStatus = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val checkButton: Button = findViewById(R.id.buttonCheck)
+
+        lifecycleScope.launch {
+            networkListener = NetworkListener()
+            networkListener.checkNetworkAvailability(this@MainActivity)
+                .collect { status ->
+                    networkStatus = status
+                    Log.d("NetworkListener", "Network status:$status")
+                    displayNetworkStatus()
+                    checkButton.setOnClickListener {
+                        showNetworkStatus()
+                    }
+                }
+        }
+    }
+
+    private fun showNetworkStatus() {
+        if (!networkStatus) {
+            Toast.makeText(this, "Disconnected", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun displayNetworkStatus() {
+        val textView = findViewById<TextView>(R.id.textDisplayCurrentInternetConnectionType)
+        if (!networkStatus) {
+            textView.text = getString(R.string.no_internet_connection)
+        } else {
+            textView.text = getString(R.string.internet_is_on)
         }
     }
 }
